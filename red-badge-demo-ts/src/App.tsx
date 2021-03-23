2cb55215ff5270f5d247b3e609ca3f3f02cd54c2
jen <jayhunt824@gmail.com>
2021-03-23 19:47:29 -0400

    first commit

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
-
+import PropsExample from "./component/PropsExample.jsx";
+import PropsMappingExample from "./component/PropsMappingExample";
 function App() {
-  return <div></div>;
+  const visitedPlaces = ["Minnesota", "Florida", "Vegas", "New York", "Chicago", "Tennesee" ];
+  return (
+    <div>
+      <PropsExample name="Tom" business="MySpace" />
+      <PropsMappingExample visited={visitedPlaces} />
+    </div>
+  );
 }
-
 export default App;
+
